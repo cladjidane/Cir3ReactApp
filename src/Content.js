4167dacdc11843eb8135e9bc22b67745834f8c89
Canu Fabien <fabien.canu@gmail.com>
2020-10-04 12:39:54 +0200

    Creation du composant prof

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Prof from './Prof'
 
 const Content = ({ text }) => (
   <div className="content">
     <p>{text}</p>
+    <Prof nom="Robert" prenom="Pirres" />
+    <Prof nom="Georges" prenom="Weha" />
+    <Prof nom="Gérard" prenom="Jirres" />
   </div>
 );
 
---
+++ src/Prof.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Prof = ({ nom, prenom }) => (
+  <div className="prof">
+    <p>{nom} - {prenom}</p>
+  </div>
+);
+
+export default Prof
