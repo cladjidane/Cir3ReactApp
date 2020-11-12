8b1f00c628d0887503d861f9f423c27a7111aa42
Canu Fabien <fabien.canu@gmail.com>
2020-11-12 22:16:56 +0100

    Update Aside.js

--- src/Components/Aside.js
+++ src/Components/Aside.js
@@ -2,9 +2,7 @@ import FetchDog from "./FetchDog";
 import React from "react";
 
 const Aside = ({ text }) => (
-  <aside className="aside">
-    <FetchDog />
-  </aside>
+  <aside className="aside">{/*<FetchDog />*/} </aside>
 );
 
 export default Aside;
